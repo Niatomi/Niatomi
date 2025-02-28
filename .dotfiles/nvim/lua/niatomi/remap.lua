1964d6bd9a366fa9b17a42efab8a7aa778faca3c
Niatomi <playervoker@gmail.com>
2025-02-28 22:09:42 +0400

    add moving block of code

--- .dotfiles/nvim/lua/niatomi/remap.lua
+++ .dotfiles/nvim/lua/niatomi/remap.lua
@@ -5,9 +5,13 @@ vim.keymap.set("n", "<leader>wh", ":wincmd h<CR>")
 vim.keymap.set("n", "<leader>wj", ":wincmd j<CR>")
 vim.keymap.set("n", "<leader>wk", ":wincmd k<CR>")
 vim.keymap.set("n", "<leader>wl", ":wincmd l<CR>")
+vim.keymap.set("n", "<leader>rls", ":LspRestart <CR>")
 
 vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>tt", ":NvimTreeToggle l<CR>")
 vim.keymap.set("n", "<leader>tsl", ":NvimTreeResize +10<CR>")
 vim.keymap.set("n", "<leader>tsh", ":NvimTreeResize -10<CR>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
