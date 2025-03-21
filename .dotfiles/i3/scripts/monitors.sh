7f098e571bd5784eb4dfdd9786779c38f02aaa12
Niatomi <playervoker@gmail.com>
2025-03-21 13:44:05 +0400

    new layout

--- .dotfiles/i3/scripts/monitors.sh
+++ .dotfiles/i3/scripts/monitors.sh
@@ -3,7 +3,8 @@
 export MONITORS_AMMOUNT=$(xrandr --query | grep -w connected | wc -l | tail -1);
 echo $MONITORS_AMMOUNT
 if [ "$MONITORS_AMMOUNT" -eq "3" ]; then
-    xrandr --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --mode 1680x1050 --pos 240x0 --rotate normal --output DP-1-2 --mode 1680x1050 --pos 1920x71 --rotate left --output DP-1-3 --off --output eDP --primary --mode 1920x1080 --pos 0x1050 --rotate normal
+    xrandr --output eDP --mode 1920x1080 --pos 1680x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --primary --mode 1680x1050 --pos 0x30 --rotate normal --output DP-1-2 --mode 1680x1050 --pos 3600x30 --rotate normal --output DP-1-3 --off
+
 fi;
 
 
