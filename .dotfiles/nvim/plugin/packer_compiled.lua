9b7b6bf2773ad567309a5f72a49919dac658c969
Niatomi <playervoker@gmail.com>
2025-07-16 02:25:43 +0400

    upd

--- .dotfiles/nvim/plugin/packer_compiled.lua
+++ .dotfiles/nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/nia/.cache/nvim/packer_hererocks/2.1.1713484068/share/lua/5.1/?.lua;/home/nia/.cache/nvim/packer_hererocks/2.1.1713484068/share/lua/5.1/?/init.lua;/home/nia/.cache/nvim/packer_hererocks/2.1.1713484068/lib/luarocks/rocks-5.1/?.lua;/home/nia/.cache/nvim/packer_hererocks/2.1.1713484068/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/nia/.cache/nvim/packer_hererocks/2.1.1713484068/lib/lua/5.1/?.so"
+local package_path_str = "/home/nia/.cache/nvim/packer_hererocks/2.1.1748459687/share/lua/5.1/?.lua;/home/nia/.cache/nvim/packer_hererocks/2.1.1748459687/share/lua/5.1/?/init.lua;/home/nia/.cache/nvim/packer_hererocks/2.1.1748459687/lib/luarocks/rocks-5.1/?.lua;/home/nia/.cache/nvim/packer_hererocks/2.1.1748459687/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/nia/.cache/nvim/packer_hererocks/2.1.1748459687/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -84,10 +84,25 @@ _G.packer_plugins = {
     path = "/home/nia/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
+  ["conform.nvim"] = {
+    loaded = true,
+    path = "/home/nia/.local/share/nvim/site/pack/packer/start/conform.nvim",
+    url = "https://github.com/stevearc/conform.nvim"
+  },
+  ["denty.nvim"] = {
+    loaded = true,
+    path = "/home/nia/.local/share/nvim/site/pack/packer/start/denty.nvim",
+    url = "https://github.com/antonk52/denty.nvim"
+  },
   ["indent-blankline.nvim"] = {
     loaded = true,
     path = "/home/nia/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
     url = "https://github.com/lukas-reineke/indent-blankline.nvim"
+  },
+  ["indentmini.nvim"] = {
+    loaded = true,
+    path = "/home/nia/.local/share/nvim/site/pack/packer/start/indentmini.nvim",
+    url = "https://github.com/nvimdev/indentmini.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
