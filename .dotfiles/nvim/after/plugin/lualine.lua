f5a22679725efbd5e577dd9e8b8a239aa40f1baa
Niatomi <playervoker@gmail.com>
2025-07-16 02:25:06 +0400

    icon change to arch

--- .dotfiles/nvim/after/plugin/lualine.lua
+++ .dotfiles/nvim/after/plugin/lualine.lua
@@ -82,14 +82,14 @@ ins_left {
   function()
     return '▊'
   end,
-  color = { fg = colors.blue }, -- Sets highlighting of component
+  color = { fg = colors.blue },      -- Sets highlighting of component
   padding = { left = 0, right = 1 }, -- We don't need space before this
 }
 
 ins_left {
   -- mode component
   function()
-    return '󰕈'
+    return '󰣇'
   end,
   color = function()
     -- auto change color according to neovims mode
@@ -169,7 +169,7 @@ ins_left {
 
 -- Add components to right sections
 ins_right {
-  'o:encoding', -- option component same as &encoding in viml
+  'o:encoding',       -- option component same as &encoding in viml
   fmt = string.upper, -- I'm not sure why it's upper case either ;)
   cond = conditions.hide_in_width,
   color = { fg = colors.green, gui = 'bold' },
